566dbdffcec34cdd7c9e5d859cc6632a5ced2dd5
ShanandhAadhithya <shanandhaadhithya2005@gmail.com>
2023-08-05 13:05:08 +0530

    add StudentList

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -11,10 +11,21 @@ public class StudentService {
     }
 
     public void addStudent(){
+        Student student =new Student();
+        List<Student> studentList = new ArrayList<student>();
+        student.setStudentId("002");
+        student.setName("Name");
+        student.setCourse("BCA");
+        student.setStartDate("05/08/2023");
+        student.setEndDate("08/01/2026");
+        studentList.add(student)
+
 
     }
 
     public List<Student> getStudents(){
+
         return new ArrayList<Student>();
+
     }
 }
